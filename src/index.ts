0894e09661b8e20e73d2845b15eae685b6817497
Murray918 <darbyguitars@gmail.com>
2022-11-09 22:56:04 -0600

    fixed neo4j authentication

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan' 
+
+dotenv.config()
+import './config/index'
 
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res, next) => {
   res.send('Hello World!')
