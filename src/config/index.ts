f8bf4dd1843321348fdb8b616b31ffc482c4f694
Murray918 <darbyguitars@gmail.com>
2022-12-06 23:15:16 -0600

    fix neo4jDriver, send persons to front-end

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,83 +1,83 @@
 import neo4j, { Driver } from 'neo4j-driver'
 
+const uri = process.env.NEO4J_URI as string
+const user = process.env.NEO4J_USER as string
+const password = process.env.NEO4J_PASSWORD as string
 
 
-(async () => {
-    const uri = process.env.NEO4J_URI as string
-    const user = process.env.NEO4J_USER as string
-    const password = process.env.NEO4J_PASSWORD as string
-
-    // To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
+export const neo4jDriver = async (fn: any) => {
     const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
-
+    // To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
     try {
-        const person1Name = 'Alice'
-        const person2Name = 'David'
-
-        await createFriendship(driver, person1Name, person2Name)
-
-        await findPerson(driver, person1Name)
-        await findPerson(driver, person2Name)
+        return await fn(driver)
     } catch (error) {
         console.error(`Something went wrong: ${error}`)
     } finally {
         // Don't forget to close the driver connection when you're finished with it.
         await driver.close()
+        console.log('🛑 = Driver closed = 🛑')
     }
+}
 
-    async function createFriendship(driver: Driver, person1Name: string, person2Name: string) {
+export async function findPersons(driver: Driver) {
 
-        // To learn more about sessions: https://neo4j.com/docs/javascript-manual/current/session-api/
-        const session = driver.session({ database: 'neo4j' })
+    const session = driver.session({ database: 'neo4j' })
 
-        try {
-            // To learn more about the Cypher syntax, see: https://neo4j.com/docs/cypher-manual/current/
-            // The Reference Card is also a good resource for keywords: https://neo4j.com/docs/cypher-refcard/current/
-            const writeQuery = `MERGE (p1:Person { name: $person1Name })
-                                    MERGE (p2:Person { name: $person2Name })
-                                    MERGE (p1)-[:KNOWS]->(p2)
-                                    RETURN p1, p2`
+    try {
+        const readQuery = `MATCH (p:Person)
+        RETURN p
+        LIMIT 100`
 
-            // Write transactions allow the driver to handle retries and transient errors.
-            const writeResult = await session.executeWrite((tx) =>
-                tx.run(writeQuery, { person1Name, person2Name })
-            )
+        const readResult = await session.executeRead(tx =>
+            tx.run(readQuery)
+        )
 
-            // Check the write results.
-            writeResult.records.forEach((record: any) => {
-                const person1Node = record.get('p1')
-                const person2Node = record.get('p2')
-                console.info(`Created friendship between: ${person1Node.properties.name}, ${person2Node.properties.name}`)
-            })
-
-        } catch (error) {
-            console.error(`Something went wrong: ${error}`)
-        } finally {
-            // Close down the session if you're not using it anymore.
-            await session.close()
-        }
+    const result = readResult.records.map(record => {
+        const person = record.get('p')
+             return {
+                name : person.properties.name,
+                born : person.properties.born 
+             }
+        })
+        return result
+    } catch (error) {
+        console.error(`Something went wrong: ${error}`)
+    } finally {
+        await session.close()
+        console.log('🛑 = Session closed = 🛑')
     }
+}
 
-    async function findPerson(driver: Driver, personName: string) {
+export async function createFriendship(driver: Driver, person1Name: string, person2Name: string) {
 
-        const session = driver.session({ database: 'neo4j' })
+    // To learn more about sessions: https://neo4j.com/docs/javascript-manual/current/session-api/
+    const session = driver.session({ database: 'neo4j' })
 
-        try {
-            const readQuery = `MATCH (p:Person)
-                                WHERE p.name = $personName
-                                RETURN p.name AS name`
+    try {
+        // To learn more about the Cypher syntax, see: https://neo4j.com/docs/cypher-manual/current/
+        // The Reference Card is also a good resource for keywords: https://neo4j.com/docs/cypher-refcard/current/
+        const writeQuery = `MERGE (p1:Person { name: $person1Name })
+                                MERGE (p2:Person { name: $person2Name })
+                                MERGE (p1)-[:KNOWS]->(p2)
+                                RETURN p1, p2`
 
-            const readResult = await session.executeRead(tx =>
-                tx.run(readQuery, { personName })
-            )
+        // Write transactions allow the driver to handle retries and transient errors.
+        const writeResult = await session.executeWrite((tx) =>
+            tx.run(writeQuery, { person1Name, person2Name })
+        )
 
-            readResult.records.forEach(record => {
-                console.log(`Found person: ${record.get('name')}`)
-            })
-        } catch (error) {
-            console.error(`Something went wrong: ${error}`)
-        } finally {
-            await session.close()
-        }
+        // Check the write results.
+        writeResult.records.forEach((record: any) => {
+            const person1Node = record.get('p1')
+            const person2Node = record.get('p2')
+            console.info(`Created friendship between: ${person1Node.properties.name}, ${person2Node.properties.name}`)
+        })
+
+    } catch (error) {
+        console.error(`Something went wrong: ${error}`)
+    } finally {
+        // Close down the session if you're not using it anymore.
+        await session.close()
+        console.log('🛑 = Session closed = 🛑')
     }
-})()
+}
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,26 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan' 
 
+
 dotenv.config()
-import './config/index'
+import { neo4jDriver, findPersons } from './config/index'
+
 
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!')
-  next
+app.get('/persons', async (req, res, next) => {
+  try {
+    const result = await neo4jDriver(findPersons)
+    res.json(result)
+  } catch(error) {
+    res.status(500).json({ error })
+  }
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
